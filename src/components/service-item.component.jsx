c300e08e811905d8f9e04c0d23f65b6eeb5913dc
DuyLuong <luonguit@gmail.com>
2019-09-01 00:46:01 +0700

    update service item layout

--- src/components/service-item.component.jsx
+++ src/components/service-item.component.jsx
@@ -1,55 +1,24 @@
-import React from 'react';
+import React from "react";
 
 class ServiceItem extends React.Component {
-
-    getImageLeft = () => {
-        return (
-            <div className="service-item">
-                <div className="row">
-                    <div className="col-sm-2">
-                        <div className="image">
-                            <img src={this.props.srcImg} alt={this.props.title} />
-                        </div>
-                    </div>
-                    <div className="col-sm-10 text-left">
-                        <a className="link" href={this.props.url}>
-                            {this.props.title}
-                        </a>
-                        <div dangerouslySetInnerHTML={{ __html: this.props.description }} />
-                        <div className="extra text-left">
-                            <a className="btn btn-primary btn-more" href={this.props.url}>
-                                Xem chi tiết
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        );
-    };
-
-    getImageRight = () => {
-        return (
-            <div className="service-item">
-                <div className="row">
-                    <div className="col-sm-9">
-                        <a className="link" href={this.props.url}>
-                            {this.props.title}
-                        </a>
-                        <div dangerouslySetInnerHTML={{ __html: this.props.description }} />
-                    </div>
-                    <div className="col-sm-3">
-                        <div className="image">
-                            <img src={this.props.srcImg} alt={this.props.title} />
-                        </div>
-                    </div>
-                </div>
-            </div>
-        );
-    };
-
-    render() {
-        return this.getImageLeft();
-    }
+  render() {
+    return (
+      <div className="service-item row">
+        <div className="col-sm-3 image">
+          <img src={this.props.srcImg} alt={this.props.title} />
+        </div>
+        <div className="col-sm-9 content">
+          <a className="link" href={this.props.url}>
+            {this.props.title}
+          </a>
+          <p className="description">{this.props.description}</p>
+          <a className="btn btn-primary btn-more" href={this.props.url}>
+            Xem chi tiết
+          </a>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default ServiceItem;
