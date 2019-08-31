972f4c41dd5cae177d242afc58c81247bb31fafc
DuyLuong <luonguit@gmail.com>
2019-08-31 22:16:10 +0700

    update sidebar auto select

--- src/components/sidebar.component.jsx
+++ src/components/sidebar.component.jsx
@@ -1,130 +1,162 @@
-import React from 'react';
-import classNames from 'classnames';
-import { Menu } from 'antd';
+import React from "react";
+import classNames from "classnames";
+import "antd/dist/antd.css";
+import { Menu } from "antd";
 
 const SubMenu = Menu.SubMenu;
 
 export default class SidebarComponent extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            collapsed: false,
-        };
-
-        this.sub = null;
-        this.flatSource = [];
-        if (props && props.src.length > 0) {
-            this.flatSource = props.src
-                .map((item, idx) => {
-                    return this.flatItem(item, idx);
-                })
-                .reduce((acc, curr) => {
-                    acc.push(...curr);
-
-                    return acc;
-                }, []);
-
-            this.getDefaultSubmenu();
-        }
-    }
-
-    getPrefixCls = () => {
-        return this.props ? this.props.prefixClass : '';
+    this.state = {
+      collapsed: false,
+      selectedKeys: [],
+      selectedSubs: []
     };
 
-    flatItem(item, idx) {
-        if (!item.children || item.children.length === 0) {
-            return [
-                {
-                    sub: `sub${idx}`,
-                    key: idx,
-                    href: item.href,
-                },
-            ];
-        }
+    this.leafKey = 1;
+    this.subKey = 1;
+  }
 
-        return item.children.map((i, idex) => {
-            return {
-                sub: `sub${idx}`,
-                key: idex + '',
-                href: i.href,
-            };
+  getPrefixCls = () => {
+    return this.props ? this.props.prefixClass : "";
+  };
+
+  buildLeaf(key, item) {
+    return (
+      <Menu.Item key={this.leafKey++}>
+        <a className="item-link" href={item.href}>
+          {item.icon != null && (
+            <span>
+              <i className={item.icon} />
+            </span>
+          )}
+          <span className="text">{item.title}</span>
+        </a>
+      </Menu.Item>
+    );
+  }
+
+  buildNode(key, item) {
+    const _self = this;
+
+    return (
+      <SubMenu
+        key={`sub${this.subKey++}`}
+        title={
+          <span>
+            {item.icon != null && (
+              <span>
+                <i className={item.icon} />
+              </span>
+            )}
+            <span className="text">{item.title}</span>
+          </span>
+        }
+      >
+        {item.children.map((child, idx) => {
+          return _self.buildLeaf(idx, child);
+        })}
+      </SubMenu>
+    );
+  }
+
+  defaultSelectedKeys = items => {
+    let node = 0,
+      leaf = 1;
+    const flatItem = item => {
+      if (item.children && item.children.length > 0) {
+        node++;
+        return item.children.map(i => {
+          return {
+            node: node,
+            key: leaf++,
+            href: i.href
+          };
         });
+      } else {
+        return [
+          {
+            node: 0,
+            key: leaf++,
+            href: item.href
+          }
+        ];
+      }
+    };
+
+    const flatSource = items
+      .map((item, idx) => {
+        return flatItem(item, idx);
+      })
+      .reduce((acc, curr) => {
+        acc.push(...curr);
+
+        return acc;
+      }, []);
+
+    const sub = flatSource.find(s => s.href === window.location.pathname);
+    if (!sub) {
+      return {
+        node: 1,
+        key: 0
+      };
     }
 
-    buildLeaf(key, item) {
-        return (
-            <Menu.Item key={key}>
-                <a className="item-link" href={item.href}>
-                    {item.icon != null && (
-                        <span>
-                            <i className={item.icon} />
-                        </span>
-                    )}
-                    <span className="text">{item.title}</span>
-                </a>
-            </Menu.Item>
-        );
+    return sub;
+  };
+
+  firstActive = () => {
+    if (this.props.src.length > 0 && this.state.selectedKeys.length === 0) {
+      const { key, node } = this.defaultSelectedKeys(this.props.src);
+      this.setState({
+        selectedKeys: [`${key}`],
+        selectedSubs: [`sub${node}`]
+      });
     }
+  };
 
-    buildNode(key, item) {
-        const _self = this;
+  onOpenChange = e => {
+    this.setState({
+      selectedSubs: e
+    });
+  };
 
-        return (
-            <SubMenu
-                key={`sub${key}`}
-                title={
-                    <span>
-                        {item.icon != null && (
-                            <span>
-                                <i className={item.icon} />
-                            </span>
-                        )}
-                        <span className="text">{item.title}</span>
-                    </span>
-                }
-            >
-                {item.children.map((child, idx) => {
-                    return _self.buildLeaf(idx, child);
-                })}
-            </SubMenu>
-        );
-    }
+  render() {
+    const props = this.props;
+    const baseClassName = classNames(
+      "sidebar",
+      this.getPrefixCls(),
+      props.className
+    );
+    const menuItems = props.src;
+    const _self = this;
+    this.leafKey = 1;
+    this.subKey = 1;
+    this.firstActive();
 
-    getDefaultSubmenu() {
-        const sub = this.flatSource.find(s => s.href === window.location.pathname);
+    return (
+      <div className={baseClassName} style={{ width: this.props.width }}>
+        <Menu
+          ref={this.primaryRef}
+          openKeys={this.state.selectedSubs}
+          selectedKeys={this.state.selectedKeys}
+          forceSubMenuRender={true}
+          mode="inline"
+          theme="dark"
+          inlineCollapsed={this.props.collapsed}
+          onOpenChange={this.onOpenChange}
+        >
+          {menuItems.map((item, idx) => {
+            if (item.children) {
+              return _self.buildNode(idx, item);
+            }
 
-        if (sub) {
-            this.sub = sub;
-        }
-    }
-
-    render() {
-        const props = this.props;
-        const baseClassName = classNames('sidebar', this.getPrefixCls(), props.className);
-        const menuItems = props.src;
-        const _self = this;
-
-        return (
-            <div className={baseClassName} style={{ width: this.props.width }}>
-                <Menu
-                    defaultSelectedKeys={this.sub != null ? [this.sub.key] : ['0']}
-                    defaultOpenKeys={this.sub != null ? [this.sub.sub] : ['sub0']}
-                    forceSubMenuRender={true}
-                    mode="inline"
-                    theme="dark"
-                    inlineCollapsed={this.props.collapsed}
-                >
-                    {menuItems.map((item, idx) => {
-                        if (item.children) {
-                            return _self.buildNode(idx, item);
-                        }
-
-                        return _self.buildLeaf(idx, item);
-                    })}
-                </Menu>
-            </div>
-        );
-    }
+            return _self.buildLeaf(idx, item);
+          })}
+        </Menu>
+      </div>
+    );
+  }
 }
